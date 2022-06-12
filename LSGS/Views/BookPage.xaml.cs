8c77f4da92bd91513135e2c13f47c20e5fd37957
İbrahim Can Gençel <ibrahimcangencel@gmail.com>
2022-06-12 23:50:42 +0300

    Comments are returned in Book_page

--- LSGS/Views/BookPage.xaml.cs
+++ LSGS/Views/BookPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using LSGS.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MySqlConnector;
 
 namespace LSGS.Views
 {
@@ -17,6 +18,39 @@ namespace LSGS.Views
         {
             BookInformation = ViewModels.SearchBookViewModel.BookSearchResultsList.Find(book => book.SerialNo == BookSearchResultsPage.BookSerialNo);
             InitializeComponent();
+            List<Comment> comment_result_list = new List<Comment>();
+
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "db4free.net",
+                UserID = "is502grp3",
+                Password = "metu2022is502",
+                Database = "lsgsg3",
+            };
+            // open a connection asynchronously
+            var connection = new MySqlConnection(builder.ConnectionString);
+            connection.Open();
+            // create a DB command and set the SQL statement with parameters
+            var command = connection.CreateCommand();
+            command.CommandText =
+                $@"SELECT *
+                  FROM Comment
+                  LEFT JOIN User ON User.METU_ID = Comment.User_ID
+                  WHERE Book_ID = {BookInformation.SerialNo}
+                  ;";
+            // execute the command and read the results
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var comment = reader.GetString("User_Comment");
+                var rating = reader.GetInt32("Rating");
+                var first_name = reader.GetString("First_Name");
+                var surname = reader.GetString("Surname");
+                var each_book = new Comment(BookInformation.SerialNo, Globals.profile.METU_ID.ToString(), first_name+" "+surname , comment, (rating).ToString());
+                comment_result_list.Add(each_book);
+            }
+            connection.Close();
             BindingContext = this.BookInformation;
         }
 
