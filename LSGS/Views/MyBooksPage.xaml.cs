c4cae59a861cf7cef2dc67f219066a5de0221953
Nisa Demir <partner.Nisa.Demir@philips.com>
2022-06-21 20:19:36 +0200

    mybookspage added

---
+++ LSGS/Views/MyBooksPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using LSGS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LSGS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyBooksPage : ContentPage
+    {
+        public IList<Book> BookList { get; set; }
+        public MyBooksPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
